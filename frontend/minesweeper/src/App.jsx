3fdf190db723055bde6e20a8fbdb8663de147514
michaelkyalo <michaelkyalo876@gmail.com>
2025-10-08 02:59:00 +0300

    add frontend

---
+++ frontend/minesweeper/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import MineSweeper from "./components/minesweeper.jsx"
+
+function App() {
+
+  return (
+    <div className="minesweeper">
+      <MineSweeper />
+    </div>
+  );
+};
+
+export default App
